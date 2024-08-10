b334d62975bc24015f2ddd223831ff585901cda2
nlok5923 <nlokhande5923@gmail.com>
2024-08-10 20:28:46 +0530

    fix readme conflicts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.25",
   networks: {
-    local: {
+    encifher: {
       url: `https://rpc.encifher.io`,
       accounts: [`0xdccad5981ea29ed47ce96ed4860c455927703771f526f6d177986a3fd14f0a36`],
       timeout: 10000000, // need to increase the timeout here 
